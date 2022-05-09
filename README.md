abe6957bc42e0a4282dacafaea65c1d413748884
Nick <72771301+SICKNICKERONI@users.noreply.github.com>
2022-05-08 20:27:04 -0600

    initial commit
    first version babyyy

---
+++ README.md
@@ -0,0 +1,55 @@
+# foxhole.js
+
+This is an **unofficial** API wrapper for the game 'Foxhole'
+You can find the game here: [Foxhole Game Steam Page](https://store.steampowered.com/app/505460/Foxhole/)
+
+## Install
+
+To use this wrapper you can install it with `npm`.
+```
+npm install foxhole.js
+```
+> Note: You must have NodeJS version 17.5 or greater to use this package as it uses then experimental NodeJS Core fetch() API
+
+## Usage
+
+```js
+const Foxhole = require('foxhole.js');
+const foxhole = new Foxhole(); // Use 'LIVE1', 'LIVE2', or 'DEV' to access the different shards. (Optionally leave blank to default to 'LIVE1')
+```
+
+### Examples
+
+This gets current war info about the war.
+```js
+const Foxhole = require('foxhole.js');
+const foxhole = new Foxhole();
+
+foxhole.getState().then((info) => {
+    console.log(info); // Alternatively, do something else here.
+});
+```
+
+This gets the current amount of casualties.
+```js
+const Foxhole = require('foxhole.js');
+const foxhole = new Foxhole();
+
+foxhole.getCasualties().then((casualties) => {
+    console.log(casualties); // Alternatively, do something else here.
+});
+```
+
+This gets an array of all the map (hex) names.
+```js
+const Foxhole = require('foxhole.js');
+const foxhole = new Foxhole();
+
+foxhole.getMaps().then((maps) => {
+    console.log(maps); // Alternatively, do something else here.
+});
+```
+
+## Extra Information
+
+*This project is in an early state and I plan to update this quite frequently.*
---
+++ index.js
@@ -0,0 +1,81 @@
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+
+class FoxholeAPI {
+    
+    /**
+     * @param {string} shard The shard you want to get info from. (e.g 'LIVE1' or leave blank, 'LIVE2', or 'DEV')
+     * 
+     * Constructor for the FoxholeAPI class.
+     */
+    constructor(shard) {
+        if (!shard || shard === 'LIVE1') {
+            this.rootURL = 'https://war-service-live.foxholeservices.com/api';
+        } else if (shard === 'LIVE2') {
+            this.rootURL = 'https://war-service-live-2.foxholeservices.com/api';
+        } else if (shard === 'DEV') {
+            this.rootURL = 'https://war-service-dev.foxholeservices.com/api';
+        } else {
+            console.log("Invalid shard! (e.g 'LIVE1' or 'LIVE2')");
+            process.exit(1);
+            return;
+        }
+    }
+
+    /**
+     * This returns a promise with information about the current war state.
+     */
+    async getState() {
+        const response = await fetch(`${this.rootURL}/worldconquest/war`);
+        const data = await response.json();
+
+        return data;
+    }
+
+    /**
+     * This returns a promise of all map names.
+     */
+    async getMaps() {
+        const response = await fetch(`${this.rootURL}/worldconquest/maps`);
+        const data = await response.json();
+
+        return data;
+    }
+
+    /**
+     * This returns a promise containing data for casualties.
+     */
+    getCasualties() {
+        let data = JSON.parse(readFileSync(path.join(__dirname, '/data/casualties.json')));
+        data.total.wardens = 0; data.total.colonials = 0; data.total.combined = 0;
+
+        const promise = new Promise((resolve) => {
+            this.getMaps().then(async (maps) => {
+                for (const map of maps) {
+                    const etag = Object.hasOwn(data.maps[map], 'etag') ? data.maps[map].etag : '';
+                    const response = await fetch(`${this.rootURL}/worldconquest/warReport/${map}`, { headers: { 'If-None-Match': etag }});
+                    console.log(response.status);
+                    if (response.status === 200) {
+                        const { wardenCasualties, colonialCasualties } = await response.json();
+                    
+                        Object.assign(data.maps, { [map]: { wardens: wardenCasualties, colonials: colonialCasualties, etag: response.headers.get('etag') } });
+                    }
+                }
+                for (const [key, value] of Object.entries(data.maps)) {
+                    data.total.wardens += value.wardens;
+                    data.total.colonials += value.colonials;
+                    data.total.combined += value.wardens + value.colonials;
+                }
+
+                const stringified = JSON.stringify(data, null, 4);
+                writeFileSync(path.join(__dirname, '/data/casualties.json'), stringified);
+
+                resolve(data);
+            });
+        });
+
+        return promise;
+    }
+}
+
+module.exports = FoxholeAPI;
